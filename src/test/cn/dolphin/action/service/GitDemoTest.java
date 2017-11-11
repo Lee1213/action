0125d94b12e88b1bbeb7cfed5dc65e09a1c3177a
李林志 <lilinzhi@lattebank.com>
2017-11-11 22:26:08 +0800

    git commit

---
+++ src/test/cn/dolphin/action/service/GitDemoTest.java
@@ -0,0 +1,18 @@
+package cn.dolphin.action.service;
+
+import org.junit.Test;
+
+/**
+ * @author LiLinZhi
+ * @date 2017-11-11 下午10:23
+ */
+public class GitDemoTest {
+
+    @Test
+    public void getCommit() {
+        System.out.println("commit1");// git commit1
+        System.out.println("commit2");// git commit2
+        System.out.println("commit2");// git commit3
+    }
+
+}
